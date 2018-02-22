b2922ef7f4988f3ffc7642f84bc7e7949505d835
BrunoTorresF <bruno.torres.fabila@gmail.com>
2018-02-21 20:23:32 -0600

    Integrate react-router, create new pages

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { BrowserRouter, Route, hashHistory } from 'react-router-dom';
 
 import Home from './pages/Home.jsx';
 import Cohort from './pages/Cohort.jsx';
@@ -10,12 +10,12 @@ import Student from './pages/Student.jsx';
 const app = document.getElementById('app');
 
 ReactDOM.render(
-  <Router history={hashHistory}>
+  <BrowserRouter history={hashHistory}>
     <Route path="/" component={Home}>
       <Route path="cohort" name="cohort1" component={Cohort}></Route>
       <Route path="Student(/:name)" name="Student" component={Student}></Route>
     </Route>
-  </Router>,
+  </BrowserRouter>,
 app);
 
 
