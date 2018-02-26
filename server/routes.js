98917470f57b55ca62d8b3579cf45160d7aa5600
Jacqueline930 <jacqueline.lopez.930@gmail.com>
2018-02-25 19:24:25 -0600

    worked on server  and added temporaly html page

--- server/routes.js
+++ server/routes.js
@@ -10,10 +10,17 @@ exports.renderQuotes = (req, res) => {
 
 //Query dB for students & mentors
 exports.renderCohort = function(req, res) {
-
+  db.getAllStudents(res => {
+    res.status(200).json(res)
+  });
+  db.getAllMentors(res => {
+    res.status(200).json(res)
+  });
 }
 
 //Query dB for single student
 exports.renderStudent= function(req, res) {
-
-}
+  db.getSingleProfile(res => {
+      res.status(200).json(res)
+    });
+  }
