d576a3d4317508784eb193616554ac479b93ebcd
Bruno Torres <33846976+BrunoTorresF@users.noreply.github.com>
2018-03-03 20:57:25 -0600

    Modify file structure, render separate views properly (#30)

--- react-client/src/components/cohort/StudentThumbnail.jsx
+++ react-client/src/components/cohort/StudentThumbnail.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const StudentThumbnail = (props) => (
-  <div style={{color: "#FEFEFE"}}>
+  <div style={{color: "purple"}}>
     <Link to={`${props.match.url}/${props.student.id}`}>
       <img id='studentsPic' className="profile-picture" src={`https://i.imgur.com/${props.student.picture_path}.jpg`}/>
     </Link>
-    <span id='studentsFN'>{`${props.student.first_name} ${props.student.last_name}`}</span>
+    <span style={{color: "purple"}} id='studentsFN'>{`${props.student.first_name} ${props.student.last_name}`}</span>
   </div>
 )
 
--- react-client/src/pages/Home.jsx
+++ react-client/src/pages/Home.jsx
@@ -2,11 +2,8 @@ import $ from "jquery";
 import React from "react";
 import {Link} from "react-router-dom";
 
-
 import SingleQuote from "../components/home/GetSingleQuote.jsx";
 import HomeGallery from "../components/home/HomeGallery.jsx";
-import HolaCode from "../components/home/HolaCode.jsx";
-import HeaderBar from "../components/home/HeaderBar.jsx";
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -17,10 +14,6 @@ export default class Home extends React.Component {
   render() {
     return (
       <div>
-          <HeaderBar />
-        <div>
-          <HolaCode />
-        </div>
         <div>
           <SingleQuote />
         </div>
