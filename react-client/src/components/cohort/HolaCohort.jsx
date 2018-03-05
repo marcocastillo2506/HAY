23d0224e3cf76ba583942acdfef19a7bfb1edb68
ElizaldeJE <javelisol@gmail.com>
2018-03-04 22:10:33 -0600

    changed the font lfor the logo in the CoHort page

--- react-client/src/components/cohort/HolaCohort.jsx
+++ react-client/src/components/cohort/HolaCohort.jsx
@@ -6,7 +6,7 @@ const HolaCoHort = (props) => (
       <div className="col-md-3">
       </div>
       <div id="contentWithinBlueWallpaperCH1" className="col-md-6">
-        <img src={'https://i.imgur.com/C7je8dQ.png'} alt="Where is the CoHort logo???" className="img-responsive-HolaCodeCH1"/>
+        <img src={'https://i.imgur.com/1oX3hkl.png'} alt="Where is the CoHort logo???" className="img-responsive-HolaCodeCH1"/>
       </div>
       <div className="col-md-3">
       </div>
