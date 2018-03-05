2aa880bb69cba0c0042d04d5a966e81b345b6ae9
ElizaldeJE <javelisol@gmail.com>
2018-03-04 19:50:09 -0600

    put the Home/Cohort tab at the top of the page in Cohort so one would be able to go backif wanted to, and forced the Cohort title to appear underneath the sticky home/cohort tab so it would appear meanwhile it gets placed properly

--- react-client/src/PrimaryLayout.jsx
+++ react-client/src/PrimaryLayout.jsx
@@ -10,14 +10,8 @@ import HeaderBar from "./components/home/HeaderBar.jsx";
 const PrimaryLayout = ({ match }) => (
   <div className="primary-layout">
     <div>
-      //deleting this since it was causing conflicts with all the other base pages,
-      //it kept duplicating information as well as traspassing to pages where it
-      //wasn't meant to be
     </div>
     <div>
-      /*deleting this since it was causing conflicts with all the other base pages,
-      it kept duplicating information as well as traspassing to pages where it
-      wasn't meant to be*/
     </div>
     <main>
       <Switch>
--- react-client/src/components/home/HolaCode.jsx
+++ react-client/src/components/home/HolaCode.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const HolaCode = (props) => (
   <section className="logo-section">
     <div className="row">
-      <div id="contentWithinBlueWallpaper" className="col-md-3">
+      <div className="col-md-3">
       </div>
       <div id="contentWithinBlueWallpaper" className="col-md-6">
         <img src={'https://i.imgur.com/1NLiQLO.png'} alt="Where is the logo???" className="img-responsive-HolaCodeYB"/>
       </div>
-      <div id="contentWithinBlueWallpaper" className="col-md-3">
+      <div className="col-md-3">
       </div>
     </div>
 </section>
