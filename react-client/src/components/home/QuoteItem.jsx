c6a1c35b71c6fcf88125403a5630d241ac104291
marcocastillo2506 <marcocastillo2506@gmail.com>
2018-03-08 19:15:33 -0600

    Started adding features to app

--- react-client/src/components/home/QuoteItem.jsx
+++ react-client/src/components/home/QuoteItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const QuoteItem = (props) => (
-  <div style={{color: "#f18973"}}>
+  <div style={{color: "#cc3300"}}>
     <span>{props.quote.quote}</span>
     <span>{props.quote.source}</span>
   </div>
